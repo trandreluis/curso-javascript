76ad311d20746769c4bf6ad3a608c9ab6ae64b16
Andre Luis <tr.andreluis@gmail.com>
2017-10-20 17:48:47 -0300

    Comparando objetos utiliando o this

---
+++ codigo-fonte/comparando-objetos/comparando-objetos.js
@@ -0,0 +1,19 @@
+var joao = {
+	nome: 'João',
+	idade: 25,
+	email: 'teste@email.com',
+	igualA: function(obj) {
+		return this.email === obj.email;
+	}
+}
+
+var maria = {
+	nome: 'Maria',
+	idade: 27,
+	email: 'teste@email.com',
+	igualA: function(obj) {
+		return this.email === obj.email;
+	}
+}
+
+console.log(joao.igualA(maria));
