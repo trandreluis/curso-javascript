23b25dcb71f269cfb695d08ad1ceb5635e4e8153
Andre Luis <tr.andreluis@gmail.com>
2017-10-20 21:54:59 -0300

    Criando arrays e iterando com forEach

---
+++ codigo-fonte/criando-arrays/criando-arrays.js
@@ -0,0 +1,18 @@
+var emails = [];
+
+emails[0] = 'joaoa@email.com';
+
+console.log(emails[0]);
+
+var nomes = ['João', 'Maria'];
+
+console.log(nomes);
+
+console.log('Tamanho:', nomes.length);
+
+nomes[4] = 'José';
+
+console.log('Quantos nomes depois do José?', nomes.length);
+
+// Espaços vazios
+console.log(nomes);
---
+++ codigo-fonte/iterando-nos-elementos-com-forEach/iterando-nos-elementos-com-forEach.js
@@ -0,0 +1,5 @@
+var nomes = ['Davi', 'Vinicius', 'André', 'Rafael', 'Franck'];
+
+nomes.forEach(function(elemento) {
+	console.log(elemento);
+});
