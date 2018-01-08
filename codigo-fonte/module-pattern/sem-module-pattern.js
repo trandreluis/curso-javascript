9e3a46b15a3b8123c24eafc5217eafa117e57efc
Andre Luis <tr.andreluis@gmail.com>
2018-01-08 11:45:00 -0300

    Aprendizado do module pattern - encapsulamento/valores privados

---
+++ codigo-fonte/module-pattern/sem-module-pattern.js
@@ -0,0 +1,16 @@
+var contador = {
+	valor: 0,
+	incrementar: function() {
+		this.valor++;
+	},
+	reset: function() {
+		console.log('Valor do contador agora: ', this.valor);
+		this.valor = 0;
+	}
+}
+
+//contador.valor = 'Algum texto';
+
+contador.incrementar();
+contador.incrementar();
+contador.reset();
